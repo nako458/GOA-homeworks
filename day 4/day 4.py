11d466769bd0420516774180bd512aae2cf250fb
nakonika12 <tevdoradzetinatini@gmail.com>
2024-03-02 23:45:40 -0800

    fourth commit

---
+++ day 4/day 4.py
@@ -0,0 +1,4 @@
+massage = print("what's your name?")
+message2 = print("how old are you?")
+message3 = print("what's your last name?")
+print("hi i am nika komshiashvili and I am 12 years old. what about you?")
